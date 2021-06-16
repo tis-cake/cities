b4dbac57c8ca3722b38db5c2469804ee59217d02
tis <tisbootis@gmail.com>
2021-06-16 19:57:20 +0300

    Мелкие правки.

--- project/src/mock/hotel.js
+++ project/src/mock/hotel.js
@@ -23,11 +23,12 @@ const getHotelMock = () => ({
   'bedrooms': getRandomIntInclusive(1, 6),
   'city': {
     'location': {
-      'latitude': `${getRandomIntInclusive(0, 60)}.${getRandomIntInclusive(10000, 99990)}`,
-      'longitude': `${getRandomIntInclusive(0, 60)}.${getRandomIntInclusive(10000, 99990)}`,
+      'latitude': `52.3${getRandomIntInclusive(0, 999)}`,
+      'longitude': `4.9${getRandomIntInclusive(0, 999)}`,
       'zoom': getRandomIntInclusive(8, 13),
     },
     'name': getRandomArrayItem(HOTEL_CITIES),
+    'popupName': getRandomArrayItem(HOTEL_TITLES), // кастомное поле
   },
   'description': getRandomArrayItem(HOTEL_DESCRIPTIONS),
   'goods': getRandomArrayItemRandomQuantity(HOTEL_GOODS),
